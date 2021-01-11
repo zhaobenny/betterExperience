ce1ea88dc433f3ad91267954ced57b626c4424f9
Benny Zhao <benny_zhao@sfu.ca>
2021-01-11 14:46:17 -0800

    Style and update on sorts

--- src/js/content_coop.js
+++ src/js/content_coop.js
@@ -1,36 +1,64 @@
-// Hardcoded values yayy
+// Hardcoded values yay
 const id = 5;
 const division = 7;
 const internal = 9;
 
-
-function updateTable(){
-	removeHeaders();
-	modifyRows();
-}
-
 function removeHeaders(){
-	var headerRow = document.querySelector("#postingsTable > thead > tr");
+	let headerRow = document.querySelector("#postingsTable > thead > tr");
 	headerRow.querySelector(`th:nth-child(${id})`).remove()
 	headerRow.querySelector(`th:nth-child(${division})`).remove()
 	headerRow.querySelector(`th:nth-child(${internal})`).remove()
 }
 
+
 function modifyRows(){
-	var rows = document.querySelectorAll("#postingsTable > tbody > tr"), i;
+	let rows = document.querySelectorAll("#postingsTable > tbody > tr"), i;
 	for (i = 0; i < rows.length; i++) {
+		rows[i].querySelector(`td:nth-child(${id + 1})`).style.cssText = "max-width: 500px; white-space: normal;"; //job title
 		rows[i].querySelector(`td:nth-child(${id})`).remove();
-		rows[i].querySelector(`td:nth-child(${division})`).remove();
+		combineDivision(rows[i]);
+		rows[i].querySelector(`td:nth-child(10)`).style.cssText = "max-width: 100px; white-space: nowrap; overflow: hidden; text-overflow: ellipsis;"; // location
 		rows[i].querySelector(`td:nth-child(${internal})`).remove();
 	}
 }
-
-
-// console.log("its is workin.")
-
-var check = document.querySelector("body > main > div.row-fluid > div > div > div > div > div >" +
-"div > div.panel.panel-default > div > table > tbody > tr > td:nth-child(2)").innerHTML;
-if (check && check === "Job no longer available"){
-		document.querySelector("body > main > div.row-fluid > div > div > div > div > div > div > div.panel.panel-default").remove();
+function combineDivision(row){
+	// eliminate division column
+	let division_str = row.querySelector(`td:nth-child(${division}) > span`).innerHTML;
+	let organization_str = row.querySelector(`td:nth-child(${division - 1}) > span`).innerHTML;
+	if (!(organization_str.includes(division_str))){
+		row.querySelector(`td:nth-child(${division - 1}) > span`).innerHTML = organization_str + " - " + division_str;
+	}
+	row.querySelector(`td:nth-child(${division})`).remove();
 }
-updateTable();
+
+function main(){
+	// Check if on postings page
+	if (document.querySelector("#postingsTablePlaceholder") == null){
+		return
+	}
+
+	removeHeaders();
+	modifyRows();
+
+	// Make sure the right (useless) element is being removed
+	var check = document.querySelector("body > main > div.row-fluid > div > div > div > div > div >" +
+	"div > div.panel.panel-default > div > table > tbody > tr > td:nth-child(2)").innerHTML;
+	if (check && check === "Job no longer available"){
+			document.querySelector("body > main > div.row-fluid > div > div > div > div > div > div > div.panel.panel-default").remove();
+	}
+
+	// Update the list when new sorting order is used
+	const target = document.querySelector("#postingsTablePlaceholder");
+	const config = { childList: true, subtree: true};
+
+	var observer = new MutationObserver(function(mutations) {
+		observer.disconnect();
+		removeHeaders();
+		modifyRows();
+		observer.observe(target, config);
+	});
+
+	observer.observe(target, config);
+}
+
+main();
